62d1f2903c69addbe65af77e59cb8555817cea4a
Kazuki MATSUDA <matsuda.kazuki@gmail.com>
2017-05-30 13:21:17 +0900

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# line-login-sample
-Sample of ID Federation with LINE Login.
+# [//github.com/line/line-login-sample-for-spring-security](https://github.com/line/line-login-sample-for-spring-security)
+Sample of ID Federation with [LINE Login](https://business.line.me/services/login) by [Spring Security](https://projects.spring.io/spring-security/).
+
+# How to run.
+
+```
+# Maven
+% mvn spring-boot:run -Dsecurity.oauth2.client.client-id=<Your Channel ID> -Dsecurity.oauth2.client.client-secret=<Your Channel Secret>
+  
+# Gradle
+% ./gradlew bootRun -Dsecurity.oauth2.client.client-id=<Your Channel ID> -Dsecurity.oauth2.client.client-secret=<Your Channel Secret>
+```
---
+++ src/main/java/com/example/LineLoginApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class LineLoginApplication {
+    public static void main(final String... args) throws Exception {
+        SpringApplication.run(LineLoginApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public Object whoami(@AuthenticationPrincipal final OAuth2Authentication oAuth2Authentication) {
+        return oAuth2Authentication;
+    }
+}
