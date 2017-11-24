98e228d9851d841f0bf0098c688f388c52c91b43
Kazuki MATSUDA <matsuda.kazuki@gmail.com>
2017-11-24 14:15:53 +0900

    Remove unnecessary (and wrong) annotation.

--- src/main/java/com/example/LineLoginApplication.java
+++ src/main/java/com/example/LineLoginApplication.java
@@ -3,7 +3,6 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,7 @@ public class LineLoginApplication {
     }
 
     @GetMapping("/")
-    public Object whoami(@AuthenticationPrincipal final OAuth2Authentication oAuth2Authentication) {
+    public Object whoami(final OAuth2Authentication oAuth2Authentication) {
         return oAuth2Authentication;
     }
 }
